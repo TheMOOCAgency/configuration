853a08f5d1aadebed7cbcc91dd256f49261b00ef
aureliencroq <aurelien.croq@themoocagency.com>
2019-04-30 19:45:08 +0200

    TheMOOCAgency repos and branches for ironwood

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -22,11 +22,11 @@ if [[ -z "${ANSIBLE_VERSION}" ]]; then
 fi
 
 if [[ -z "${CONFIGURATION_REPO}" ]]; then
-  CONFIGURATION_REPO="https://github.com/edx/configuration.git"
+  CONFIGURATION_REPO="https://github.com/TheMOOCAgency/configuration.git"
 fi
 
 if [[ -z "${CONFIGURATION_VERSION}" ]]; then
-    CONFIGURATION_VERSION=${OPENEDX_RELEASE-master}
+    CONFIGURATION_VERSION="ironwood.1_tma"
 fi
 
 if [[ -z "${UPGRADE_OS}" ]]; then
